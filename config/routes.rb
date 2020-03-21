449c5b268d4a8bffe64eef7bb265da6a90bb9f3a
SteveFenn <steve.fenn@immersivelabs.co.uk>
2020-03-21 15:20:22 +0000

    Initial admin scaffold for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin
+
+  namespace :admin do
+    resources :pages, except: [:show]
+  end
+
   resources :contacts, only: [:new, :create]
 
   root to: "contacts#new"
